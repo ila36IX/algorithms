d0f13b08e6d241cd83128767ca5df50f8065306d
ila36IX <jbariali002@gmail.com>
2023-11-21 01:14:20 +0000

    〽 Sorting algorithms

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -26,7 +26,7 @@ void _swap(int *a, int *b)
 int partition(int *array, int low, int high)
 {
 	int pivot = array[high];
-	int i = low - 1;
+	int i = low - 1, j;
 	static int *list;
 	static size_t size;
 
@@ -35,7 +35,7 @@ int partition(int *array, int low, int high)
 		list = array;
 		size = high + 1;
 	}
-	for (int j = low; j < high; j++)
+	for (j = low; j < high; j++)
 	{
 		if (array[j] <= pivot)
 		{
